996a0bedb7c46f65261cf298b6959370c15c56aa
S.F <1074260090@qq.com>
2019-08-13 13:15:48 +0800

    demos

--- algorithm.js
+++ algorithm.js
@@ -1,23 +1,12 @@
-var foo = {
-    value: 1
+function foo(something) {
+    console.log(this.a, something);
+    return this.a + something;
+}
+var obj = {
+    a: 2
 };
-
-function bar(name,age) {
-    console.log(this.value);
-    console.log(name);
-    console.log(age);
-}
-
-
-
-Function.prototype.mycall = function (context) {
-    context.fn = this;
-    var args = [];
-    for (var i = 1, len = arguments.length; i < len; i++) {
-        args.push('arguments[' + i + ']');
-    }
-    eval('context.fn(' + args + ')');
-    delete context.fn;
-}
-
-bar.mycall(foo,'kevin',18);
+var bar = function () {
+    return foo.apply(obj, arguments);
+};
+var b = bar(3); // 2 3
+console.log(b); // 5
