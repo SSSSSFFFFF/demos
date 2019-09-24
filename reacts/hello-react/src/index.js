0424b9a097c38214855a557cea0f713a1bfa43c2
S.F <1074260090@qq.com>
2019-09-24 17:56:22 +0800

    new-deo

--- reacts/hello-react/src/index.js
+++ reacts/hello-react/src/index.js
@@ -1,62 +1,19 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import CommentInput from './CommentInput'
+import CommentList from './CommentList'
 import './index.css'
-class LikeButton extends Component {
-  static defaultProps = {
-    likedText: '取消',
-    unlikedText: '点赞'
-  }
-
-  constructor() {
-    super()
-    this.state = { isLiked: false }
-  }
-
-  handleClickOnLikeButton() {
-    this.setState({
-      isLiked: !this.state.isLiked
-    })
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClickOnLikeButton.bind(this)}>
-        {this.state.isLiked ? this.props.likedText : this.props.unlikedText} 👍
-      </button>
-    )
-  }
-}
 class Index extends Component {
-  constructor() {
-    super()
-    this.state = {
-      likedText: '已赞',
-      unlikedText: '赞'
-    }
-  }
-
-  handleClickOnChange() {
-    this.setState({
-      likedText: '取消',
-      unlikedText: '点赞'
-    })
-  }
-
   render() {
     return (
-      <div>
-        <LikeButton
-          likedText={this.state.likedText}
-          unlikedText={this.state.unlikedText} />
-        <div>
-          <button onClick={this.handleClickOnChange.bind(this)}>
-            修改 wordings
-          </button>
-        </div>
+      <div className="wrapper">
+        <CommentInput />
+        <CommentList />
       </div>
     )
   }
 }
+
 ReactDOM.render(
   <Index />,
   document.getElementById('root')
