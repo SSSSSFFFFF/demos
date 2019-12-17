12c65e7014f434d21c5c0c386c2fa3602bb40e68
S.F <1074260090@qq.com>
2019-12-17 11:31:32 +0800

    groupBy

--- algorithm.js
+++ algorithm.js
@@ -1,4 +1,31 @@
 
+function groupBy(arr,para){
+    let obj ={};
+    if(typeof para == 'function'){
+        arr.map(i => {
+            let str = para(i).toString()
+            arrayMap(str, i)
+        })
+    } else if (typeof para == 'string') {
+        arr.map(i => {
+            let len =  i.length
+            arrayMap(len, i)
+        })
+    }
+
+    function arrayMap(len,i){
+        if (!obj.hasOwnProperty(len)) {
+            obj[len] = [i]
+        } else {
+            obj[len].push(i)
+        }
+    }
+    console.log(obj);
+}
+
+// groupBy([6.1, 4.2, 6.3], Math.floor)
+// groupBy(['one','two','three'], 'length')
+
 //=>[1,2,3,45]
 function flattenDeep(arr){
     let newArr = [];
@@ -12,10 +39,8 @@ function flattenDeep(arr){
             }
         })
     }
-    
-
     console.log(newArr);
 }
 
 
-flattenDeep([1, [2, [3, 4], 5]])
+// flattenDeep([1, [2, [3, 4], 5]])
