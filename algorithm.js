297d13611fb303d9eaf7a4b3c98d667d6eda347d
S.F <1074260090@qq.com>
2019-12-18 18:05:48 +0800

    require

--- algorithm.js
+++ algorithm.js
@@ -1,20 +1,19 @@
 
-
-console.log(quickSort([3, 14, 5, 4, 54, 5435, 442, 4321]));
+// console.log(quickSort([3, 14, 5, 4, 54, 5435, 442, 4321]));
+quickSort([3, 14, 5, 4, 54, 5435, 442, 4321])
 function quickSort(arr) {
     if (arr.length <= 1) { return arr; }
-    var pivotIndex = Math.floor(arr.length / 2);
-    var pivot = arr.splice(pivotIndex, 1)[0];
-    var left = [];
-    var right = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
+    let index = Math.floor(arr.length / 2) 
+    let num = arr.splice(index,1)[0]
+    let left = [], right= [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < num){
+            left.push(arr[i])
         } else {
-            right.push(arr[i]);
+            right.push(arr[i])
         }
     }
-    return quickSort(left).concat([pivot], quickSort(right));
+    return quickSort(left).concat([num], quickSort(right));
 };
 //插入排序
 function InsertionSort(array){
