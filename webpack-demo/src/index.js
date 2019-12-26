f1149a6fcdcbf36c3d5580675642ac29c02383e6
S.F <1074260090@qq.com>
2019-12-26 15:50:04 +0800

    webpack精通

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,20 @@
+import './style.css'
+// import MyImage from './my-image.png'
+import printMe from './print.js';
+
+
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
         // Lodash, now imported by this script
-        element.innerHTML = 'hello webpack!'
+    element.innerHTML = 'hello webpack!'
+    element.classList.add('hello');
+    var img= new Image();
+    // img.src = MyImage
+    element.appendChild(img)
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;     
+    element.appendChild(btn);
     return element;
 }
 
---
+++ webpack-demo/src/print.js
@@ -0,0 +1,4 @@
+export default function printMe() {
+    console.log("clicked121");
+    console.error('I get called from print.js!');
+}
